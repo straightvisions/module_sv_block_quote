4fb82baeb1cd07ceaebd81431644e00bbc982b89
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-06-30 17:05:19 +0200

    remove default values

--- sv_block_quote.php
+++ sv_block_quote.php
@@ -28,7 +28,6 @@
 			$this->get_setting( 'font_size' )
 				->set_title( __( 'Font Size', 'sv100' ) )
 				->set_description( __( 'Font Size in pixel.', 'sv100' ) )
-				->set_default_value( 16 )
 				->set_is_responsive(true)
 				->load_type( 'number' );
 
@@ -81,7 +80,6 @@
 			$this->get_setting( 'cite_font_size' )
 				->set_title( __( 'Font Size', 'sv100' ) )
 				->set_description( __( 'Font Size in pixel.', 'sv100' ) )
-				->set_default_value( 16 )
 				->set_is_responsive(true)
 				->load_type( 'number' );
 
